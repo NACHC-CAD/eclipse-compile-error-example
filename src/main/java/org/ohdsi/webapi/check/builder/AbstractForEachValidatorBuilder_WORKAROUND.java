1964c5a573d8e8a95c921e0e036b47827dabc319
greshje <greshje@gmail.com>
2022-10-04 09:51:49 -0400

    added workaround solution

---
+++ src/main/java/org/ohdsi/webapi/check/builder/AbstractForEachValidatorBuilder_WORKAROUND.java
@@ -0,0 +1,21 @@
+package org.ohdsi.webapi.check.builder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.ohdsi.webapi.check.validator.ValidatorGroup;
+
+public abstract class AbstractForEachValidatorBuilder_WORKAROUND<T, V, X> extends ValidatorBuilder<V> {
+
+	private List<ValidatorGroupBuilder<T, X>> validatorGroupBuilders = new ArrayList<ValidatorGroupBuilder<T, X>>();
+
+	protected List<ValidatorGroup<T, X>> initGroups() {
+		return initAndBuildList(this.validatorGroupBuilders);
+	}
+
+    private <U> List<U> initAndBuildList(List<? extends ValidatorBaseBuilder<T, U, ?>> builders) {
+    	return null;
+    }
+
+}
+
---
+++ src/main/java/org/ohdsi/webapi/check/builder/foo.java
@@ -0,0 +1,5 @@
+package org.ohdsi.webapi.check.builder;
+
+public class foo {
+
+}
